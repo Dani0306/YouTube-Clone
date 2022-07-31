500dd6a8fdec86446969b67af82b53264f2b6300
Daniel Colorado <danicolorado0306@gmail.com>
2022-07-31 13:13:51 -0500

    last commit

--- src/components/VideoPlayerRelated.jsx
+++ src/components/VideoPlayerRelated.jsx
@@ -7,22 +7,24 @@ const VideoPlayerRelated = ({ relatedLoading }) => {
   const relatedVideos = useSelector(state => state.videos.relatedVideos)
 
 
-  if (relatedLoading) return (
-    <div className="w-ful h-screen flex items-center justify-center">
-      <TailSpin color="#fff" widht={60} height={60}/>
-    </div>
-  )
-
   return (
     <div className='videoplayer-related scrollbar-hide lg:w-[30%] lg:flex lg:flex-col'>
       {
-        relatedVideos.map((item, index) => {
-          const { items } = item;
-          const { contentDetails: { duration }, snippet: {  title, channelTitle, publishedAt, thumbnails: { high: { url } } }, statistics: { viewCount, } } = items[0];
-          return (
-            <VideoRelatedVideoComponent key={index} duration={duration} channelTitle={channelTitle} publishedAt={publishedAt} viewCount={viewCount} title={title} url={url}/>
-          )            
-        })
+          relatedLoading ? 
+          
+          <div className="w-full h-screen flex items-center justify-center">
+            <div className="mt-[-150px]">
+              <TailSpin color="#fff" widht={60} height={60}/>
+            </div>
+          </div> 
+          
+          : 
+        
+          relatedVideos.map((item, index) => {
+              const { items } = item;
+              const { contentDetails: { duration }, snippet: {  title, channelTitle, publishedAt, thumbnails: { high: { url } } }, statistics: { viewCount, } } = items[0];
+              return <VideoRelatedVideoComponent key={index} duration={duration} channelTitle={channelTitle} publishedAt={publishedAt} viewCount={viewCount} title={title} url={url}/>
+          })
       }
     </div>
   )
