ba5d1964fcff2bc78283041e2e69e5b53e9ec85a
Daniel Colorado <danicolorado0306@gmail.com>
2022-07-31 18:09:39 -0500

    last commit

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,5 @@
-import { compose, createStore, applyMiddleware } from "redux";
-
-import logger from 'redux-logger'
+import { createStore } from "redux";
 import { rootReducer } from "./root-reducer";
 
 
-const middlewares = [logger]
-
-const composedEnhancers = compose(applyMiddleware(...middlewares))
-
-export const store = createStore(rootReducer, undefined, composedEnhancers)
+export const store = createStore(rootReducer)
